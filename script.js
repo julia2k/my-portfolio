06144a906c82780a478a3cc0c457adb724576eb5
Yuliana Kutepova <40539564+julia2k@users.noreply.github.com>
2019-05-17 16:22:10 -0500

    link and anchor animation

--- script.js
+++ script.js
@@ -35,15 +35,54 @@ splitChar.each(function(i){
 	});
 });
 	
+// close Button	
 $('#welcome-button-close').hide().delay(2000).show('slide', {direction: 'right'}, 1000);
-
-
 function closeWelcomePage(){
 	$("#main-foto").hide("blind", {direction: 'up'}, 1000);
     $('#wrapper').css("display","block");
 	
 }
-$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-  e.target // newly activated tab
-  e.relatedTarget // previous active tab
-})
+
+// active link color setup
+
+function setLinkActive(elem){
+   $("a.a-link").removeClass("a-link");
+   $(elem).addClass('a-link'); 
+}
+
+function scrollAnimate(elem){
+   var link_href=$(elem).attr("href");
+   if ( link_href == "#my-works"){
+     $("html").animate({ scrollTop: $(window).height() - $("nav").height()-50 }, "slow");
+   } else if(link_href == "#hello-block"){
+   	 $("html").animate({ scrollTop: $('#hello-block').offset().top -10 }, "slow");
+   } else{
+   	$("html").animate({ scrollTop: $('#social').offset().top -$(window).height()+10 }, "slow");
+   }
+}
+
+
+$('a.nav-link').click(function () {
+   setLinkActive(this);
+   scrollAnimate(this);
+});
+
+
+//navigation links activation on scroll
+$(document).on('scroll', function() {
+	 var window_top = $(window).scrollTop();
+	 var helloBlock_top = $('#hello-block').offset().top -10;
+	 var myWorks_top = $('#my-works').offset().top -300;
+	 var social_top = $('#social').offset().top -$(window).height()+10;
+   
+	 if (window_top > social_top) {
+                setLinkActive($('a[href="#social"]'));
+                }
+	  else if (window_top > myWorks_top) {
+                setLinkActive($('a[href="#my-works"]'));
+                } else {
+                 setLinkActive($('a[href="#hello-block"]'));
+                }
+});
+
+
